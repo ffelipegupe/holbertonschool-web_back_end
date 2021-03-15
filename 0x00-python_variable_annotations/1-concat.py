4ac5627998ce76ff118b1254e03406fa65181127
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-15 18:48:16 -0500

    Add: adding file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Function that takes twos strings as arguments
+        and returns a concatenated string """
+    return str1 + str2
