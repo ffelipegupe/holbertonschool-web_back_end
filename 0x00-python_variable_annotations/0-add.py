1eda35e164fbd6e804a8234bbb39ca77c403cb7b
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-15 18:45:11 -0500

    Edit: editing function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,8 +5,3 @@
 def add(a: float, b: float) -> float:
     """ Fucntion that returns the sum of two floats """
     return a + b
-
-
-
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
