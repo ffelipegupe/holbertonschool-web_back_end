544ab614526bfae184068d036c940b26cd7de56d
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-15 19:10:43 -0500

    Edit: editing linting

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,6 +3,7 @@
 
 import typing
 
+
 def sum_list(input_list: typing.List[float]) -> float:
     """ function sum_list which takes a list input_list
         of floats as argument and returns their sum as a float """
