51f6327def13106f04a9894ba167967c2916560d
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-16 19:30:08 -0500

    Edit: editing variable annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """  async routine called wait_n that takes in 2 int arguments
         (in this order): n and max_delay. You will spawn wait_random n times
         with the specified max_delay. """
-    delays = List[float] = []
+    delays: List[float] = []
     for _ in range(n):
         delays.append(wait_random(max_delay))
     return [await delay for delay in asyncio.as_completed(delays)]
