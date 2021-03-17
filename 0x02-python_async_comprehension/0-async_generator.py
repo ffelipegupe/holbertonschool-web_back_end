aad14bf9ffac22387aca43d78a34b81044f9edea
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-17 13:19:20 -0500

    Add: adding task file

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Async Generator """
+import random
+import asyncio
+from typing import Generator
+
+
+async def asyn_generator() -> Generator[float, None, None]:
+    """ loops 10 times, each time asynchronously waits 1 second,
+        then yields a random number between 0 and 10. """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(10)
