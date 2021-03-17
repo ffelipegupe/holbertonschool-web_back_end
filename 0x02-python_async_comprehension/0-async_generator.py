aa7963e833858260ddb80f01c160444c919c376b
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-17 13:20:49 -0500

    Edit: editing uniform arguments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def asyn_generator() -> Generator[float, None, None]:
         then yields a random number between 0 and 10. """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(10)
+        yield random.uniform(0, 10)
