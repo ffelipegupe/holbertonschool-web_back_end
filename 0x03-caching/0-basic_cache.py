80a9213495df6f2e86dcee1ba7ae641e8a0e1a87
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-26 12:16:48 -0500

    Edit: functions

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """ Basic dictionary """
 
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
     """ Class BasicCache that inherits from BaseCaching and is a caching system """
     def put(self, key, item):
+        """ Adding item """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """ Getting an item """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
