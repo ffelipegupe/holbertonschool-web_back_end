e0f7fc1299c8fbb980abbabed5fcf0c0fb9cd372
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-30 18:14:46 -0500

    Add: adding all functions

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env 
 """ Regex-ing """
 import re
+import logging
+import os
+import mysql.connector
 from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -11,3 +17,55 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr"{f}=.+?{separator}",
                          f'{f}={redaction}{separator}', message)
     return message
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """constructor"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """returns a logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to a database"""
+    connector = mysql.connector.connect(
+        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.environ.get("PERSONAL_DATA_DB_NAME"))
+    return connector
+
+
+if __name__ == '__main__':
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    query = ("SELECT * FROM users")
+    cursor.execute(query)
+    for row in cursor:
+        string = ""
+        for key in row:
+            string += "{}={}; ".format(key, row[key])
+        print(string)
+    cursor.close()
+    db.close()
