00a70e9539b758139c3ffb63e0fa20374bdd94da
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-30 18:11:40 -0500

    Edit: editing function

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,8 +5,8 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str)-> str:
-    """ Function that returns the log message obfuscated """
+                 separator: str) -> str:
+    """Function that returns the log message obfuscated"""
     for f in fields:
         message = re.sub(fr"{f}=.+?{separator}",
                          f'{f}={redaction}{separator}', message)
