fbfc9555f67dd6e3750b1f4ba28733a96c433c6f
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-31 18:46:48 -0500

    Add: adding task file

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized',  methods=['GET'], strict_slashes=False)
-def unath() -> str:
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
-      - 401 error handler
+      - 401 error hanlder execution
     """
     abort(401)
