d8c376a7d920737ed9505676cb1797b0d100f3f5
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-31 19:09:25 -0500

    Update: updating require_auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,15 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth method
         """
-        return False
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if path.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header method
