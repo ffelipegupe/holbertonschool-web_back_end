2872a7df702b9ed690a52aa56552d6374aec0140
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-31 19:26:04 -0500

    Add: adding task file

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,3 +7,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class body
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Method that returns the Base64 part of the
+            Authorization header for a Basic Authentication
+        """
+        if authorization_header is None or\
+           type(authorization_header) is not str:
+            return None
+        head = authorization_header.split(' ')
+
+        return head[1] if head[0] == 'Basic' else None
