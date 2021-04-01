71b401964ce4ca6ab5dd0713bd52e0b3ead46e85
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-31 19:33:35 -0500

    Update: adding credentials function

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,19 @@ class BasicAuth(Auth):
             return (None, None)
         extract = decoded_base64_authorization_header.split(':', 1)
         return (extract[0], extract[1]) if extract else (None, None)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ Method that returns the User instance
+            based on his email and password
+        """
+        if not user_email or not isinstance(user_email, str)\
+           or not user_pwd or not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
