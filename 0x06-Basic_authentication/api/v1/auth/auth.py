78682736a65b2a49b40ac07b6463d666846307d0
Felipe Gutierrez <1560@holbertonschool.com>
2021-03-31 19:04:05 -0500

    Add: adding auth

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" API authentication 
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth method
+        """
+        return false
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header method
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+        """
+        return None
