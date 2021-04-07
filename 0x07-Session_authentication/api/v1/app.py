5fccca18d866488ff6e2327e55e2ddb9db40e27d
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-07 13:05:57 -0500

    Update: updating before function

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -49,20 +49,18 @@ def forbidden(error) -> str:
 def before_request() -> None:
     """ Before request
     """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-             '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    if not auth:
-        return None
-    if not auth.require_auth(request.path, paths):
-        return None
-    if not auth.authorization_header(request):
-        abort(401)
-    if not auth.current_user(request):
-        abort(403)
-    if not auth.authorization_header(request) and not\
-            auth.session_cookie(request):
-        abort(401)
-    request.current_user = auth.current_user(request)
+    if auth is not None:
+        exclude_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+        require_auth = auth.require_auth(path=request.path,
+                                         excluded_paths=exclude_paths)
+        if require_auth:
+            if not auth.authorization_header(request) and not \
+               auth.session_cookie(request):
+                abort(401)
+            if not auth.current_user(request):
+                abort(403)
+            request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
