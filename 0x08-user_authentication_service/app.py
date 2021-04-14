9c0018e9d5304cd51c8f0669cae710d34002788d
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 18:46:49 -0500

    Update: adding register method

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Flask App module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -11,6 +13,18 @@ app = Flask(__name__)
 def message() -> str:
     """ Method that returns a JSON payload """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register():
+    """ Registers a User """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
