cead37fcfc78bcfb0f05af7d5cb63bbda28d877a
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 17:03:52 -0500

    Update: docs

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""DB module
+""" Module of class DB
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +11,7 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """ Definition of DB class 
     """
 
     def __init__(self) -> None:
