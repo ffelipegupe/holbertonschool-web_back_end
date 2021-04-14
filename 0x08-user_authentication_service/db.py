ab172568618faa059ed73e0959c747c65f5bcacd
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 18:22:30 -0500

    Edit: annotations

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,7 +3,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
@@ -20,7 +19,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
