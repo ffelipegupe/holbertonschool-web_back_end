97f61b84c34eb556c083339de6866cc0952c0524
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 01:33:48 -0500

    Update: raising exceptions

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs) -> object:
+    def find_user_by(self, **kwargs) -> User:
         """ This method takes in arbitrary keyword arguments and returns the
             first row found in the users table as filtered by the method’s
             input arguments.
