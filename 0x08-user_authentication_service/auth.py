d7a24652a75874ad38b78625a5de1e904823fa55
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 18:33:27 -0500

    Update: adding class body

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,11 +2,31 @@
 """ Authorization module
 """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> str:
     """ method that takes in a password string arguments and returns bytes.
-        The returned bytes is a salted hash of the input password,
-        hashed with bcrypt.hashpw.
-    """
+            The returned bytes is a salted hash of the input password,
+            hashed with bcrypt.hashpw.
+        """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Auth Constructor """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method to register an User """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
