ea777be6c7d3041d9b01402d1deb3778e84a5f53
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 18:56:22 -0500

    Feature: adding _generate_uuid method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,14 +5,20 @@ import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
-    """ method that takes in a password string arguments and returns bytes.
-            The returned bytes is a salted hash of the input password,
-            hashed with bcrypt.hashpw.
-        """
+    """ Method that takes in a password string arguments and returns bytes.
+        The returned bytes is a salted hash of the input password,
+        hashed with bcrypt.hashpw.
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Function that returns a string representation of a new UUID """
+    return str(uuid4())
 
 
 class Auth:
