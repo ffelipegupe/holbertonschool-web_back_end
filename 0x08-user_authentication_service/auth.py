0df5ee20d8f0633fa3c3c1ad99859243c2b81ce8
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-14 19:18:12 -0500

    Update: adding get_user_from_session_id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ Method that gets a user from its session_id """
+        if not session_id:
+            return None
+        try:
+            src = self._db.find_user_by(session_id=session_id)
+            return src
+        except NoResultFound:
+            None
