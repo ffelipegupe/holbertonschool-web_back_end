f8f997e2d7101c812d8c9c05a58da3ead73fa3a3
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-19 21:20:20 -0500

    Update: test_org

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 """ client.py test module """
-from client import org
 from unittest.mock import Mock, patch
 from client import GithubOrgClient
 from parameterized import parameterized
 from unittest import mock
 import unittest
-from utils import get_json
 
 
 class TestGithubOrgClient(unittest.TestCase):
