291127eba5e0d59efb878269e8e6b1ddd5d732cb
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-19 21:23:52 -0500

    Feature: test_public_repos_url implementation

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ client.py test module """
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 from unittest import mock
@@ -20,3 +20,13 @@ class TestGithubOrgClient(unittest.TestCase):
         gitcli = GithubOrgClient(name)
         gitcli.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{name}')
+
+    def test_public_repos_url(self):
+        """ Method that tests _public_repos_url function """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
