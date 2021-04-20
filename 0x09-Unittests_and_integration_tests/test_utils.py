3318f049f4deaf01df89406cc24b7c487a09c3e6
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-19 19:55:14 -0500

    Add: adding test_utils.py

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" utils.py test module
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ access_nested_map test class"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Method that tests access_nested_map function """
+        self.assertEquals(access_nested_map(nested_map, path), expected)
