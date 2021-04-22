b400997233bbcb4457ca03d20e30d6108f60bff1
Felipe Gutierrez <1560@holbertonschool.com>
2021-04-21 23:37:55 -0500

    Add: adding files

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -26,4 +26,4 @@ def index() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
--- 0x0A-i18n/4-app.py
+++ 0x0A-i18n/4-app.py
@@ -16,19 +16,22 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object('3-app.Config')
+app.config.from_object('4-app.Config')
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def index() -> str:
     """ Home rendering """
-    return render_template("3-index.html")
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
     """ Function to determine the best match with our supported languages.
     """
+    locale = request.args.get("locale")
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
