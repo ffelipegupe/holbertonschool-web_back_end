7161dc5ef10ea9139d8f5be83faef724b833fa33
Felipe Gutierrez <1560@holbertonschool.com>
2021-05-06 22:05:03 -0500

    Add: adding task file

---
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+xport default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+
+    if (trueOrFalse) {
+	/* eslint-disable */
+	const task = true;
+	const task2 = false;
+	/* eslint-enable */
+    }
+
+    return [task, task2];
+}
